48e1eb84cb1fe372cdb094deaacc90a133bb51f4
SD <antongorynya1@gmail.com>
2023-03-23 23:14:33 +0300

    updatate pokemon page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,12 +2,12 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     id = models.AutoField(auto_created=True,  primary_key=True)
-    title_ru = models.CharField('Название на русском', max_length=200)
+    title_ru = models.CharField('Название на русском', max_length=200, default='Имя покемона')
     title_en = models.CharField('Название на английском', max_length=200, blank=True)
     title_jp = models.CharField('Название на японском', max_length=200, blank=True)
     description = models.TextField(default='Описание покемона')
-    next_evolution = models.IntegerField('ID В кого эволюционирует ', null=True)
-    previous_evolution = models.IntegerField('ID Из В кого эволюционирует', null=True)
+    next_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='previous', null=True, blank=True)
+    previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='next', null=True, blank=True)
     image = models.ImageField(upload_to='pokemons', default='pokemons/default.png')
 
     def __str__(self):
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -64,13 +64,12 @@ def show_pokemon(request, pokemon_id):
     pokemons = PokemonEntity.objects.filter(
         appeared_at__lt=today,
         disappeared_at__gt=today,
-        pokemon_type__id=pokemon_id)
-
+        pokemon_type__id=pokemon_id
+    )
     if not pokemons:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
     requested_pokemon_type = Pokemon.objects.get(id=pokemon_id)
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
         url = pokemon.pokemon_type.image.path
@@ -79,27 +78,40 @@ def show_pokemon(request, pokemon_id):
             pokemon.lon,
             url
         )
-
-
-
-
+    pokemon_context = {
+        'pokemon_id': requested_pokemon_type.id,
+        'title_ru': requested_pokemon_type.title_ru,
+        'title_en': requested_pokemon_type.title_en,
+        'title_jp': requested_pokemon_type.title_jp,
+        'description': requested_pokemon_type.description,
+        'img_url':  request.build_absolute_uri(requested_pokemon_type.image.url),
+    }
+    if requested_pokemon_type.previous_evolution:
+        pokemon_context.update(
+            {
+                'previous_evolution': {
+                    'pokemon_id': requested_pokemon_type.previous_evolution.id,
+                    'title_ru': requested_pokemon_type.previous_evolution.title_ru,
+                    'title_en': requested_pokemon_type.previous_evolution.title_en,
+                    'title_jp': requested_pokemon_type.previous_evolution.title_jp,
+                    'description': requested_pokemon_type.previous_evolution.description,
+                    'img_url': request.build_absolute_uri(requested_pokemon_type.previous_evolution.image.url),
+                }
+            }
+        )
+    if requested_pokemon_type.next_evolution:
+        pokemon_context.update(
+            {
+                'next_evolution': {
+                    'pokemon_id': requested_pokemon_type.next_evolution.id,
+                    'title_ru': requested_pokemon_type.next_evolution.title_ru,
+                    'title_en': requested_pokemon_type.next_evolution.title_en,
+                    'title_jp': requested_pokemon_type.next_evolution.title_jp,
+                    'description': requested_pokemon_type.next_evolution.description,
+                    'img_url': request.build_absolute_uri(requested_pokemon_type.next_evolution.image.url),
+                }
+            }
+        )
     return render(request, 'pokemon.html', context={
-        'map': folium_map._repr_html_(), 'pokemon': requested_pokemon_type
+        'map': folium_map._repr_html_(), 'pokemon': pokemon_context
     })
-
-'''
-{'pokemon_id': 2,
- 'title_ru': 'Ивизавр',
- 'title_en': 'Ivysaur',
- 'title_jp': 'フシギソウ',
- 'description': 'покемон двойного травяного и ядовитого типа из первого поколения покемонов. Эволюционирует из стартового покемона Бульбазавра на 16 уровне. С
-тановится Венузавром на 32 уровне.',
- 'img_url': 'https://vignette.wikia.nocookie.net/pokemon/images/7/73/002Ivysaur.png/revision/latest/scale-to-width-down/200?cb=20150703180624&path-prefix=ru', 
- 'entities': [{'level': 44, 'lat': 55.729472, 'lon': 3
-7.692479}, {'level': 24, 'lat': 55.730141, 'lon': 37.653911}], 
-'next_evolution': {'title_ru': 'Венузавр', 'pokemon_id': 3, 'img_url': 'https://vignette.wikia.nocookie.net/pokemon/images/a/ae/003Venusaur.png/revision/latest/scale-to-width-down/200?c
-b=20150703175822&path-prefix=ru'}, 
-'previous_evolution': {'title_ru': 'Бульбазавр', 'pokemon_id': 1, 'img_url': 'https://upload.wikimedia.org/wikipedia/ru/c/ca/%D0%91%D1%83%D0%BB%D1%8C%D0%B1%D0%B0%D0%B7%D0%B0%D0%B2%D1%80.png'}}
-
-
-'''
